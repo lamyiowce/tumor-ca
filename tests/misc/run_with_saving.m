163ce916bc850de05252bce3950d390f868101c0
Julia Bazińska <julia@bazinski.com>
2018-12-09 15:33:21 +0100

    Script fix and generated data

--- tests/misc/run_with_saving.m
+++ tests/misc/run_with_saving.m
@@ -5,7 +5,7 @@ clear global
 global P_EVO POP GEN LIB
 
 % // load runfile
-evalfile('example/runfile_example_static.txt')		% provides P_EVO
+evalfile('example/runfile_example_static.txt')      % provides P_EVO
 
 % // Define the random seed, and control for replication
 %    .. we use MATLAB's 'mt19937ar' stream ("Mersenne
@@ -18,11 +18,11 @@ P_EVO.randstreamseed = s;
 BM = GetBenchmarks(P_EVO);
 
 % // Build initial population
-GetInitialPop		        % makes POP
+GetInitialPop              % makes POP
 res  = InitializeEvoRun;
-RunStats = InitializeRunStats(P_EVO);		    % initialise output, produces RunStats
+RunStats = InitializeRunStats(P_EVO);          % initialise output, produces RunStats
 
-        tr = 1;
+tr = 1;
 sl = 1;
 gn = 1;
 
@@ -32,31 +32,32 @@ ImportTumorState(sl,tr,P_EVO,POP);
 global FINISH PARAMS PrC PrP STATE
 
 FINISH = 0;
-tumor_name = sprintf('out-%s-tr%.0f-',P_EVO.tum_lib_s,tr);
-save_workspace(strcat(tumor_name, 'initial.json'));
+st = 0;
+tumor_name = sprintf('out-%s-tr%.0f-st%.0f-%s.json',P_EVO.tum_lib_s, tr, st);
+save_workspace(sprintf('out-%s-tr%.0f-st%.0f-0a-%s.json',P_EVO.tum_lib_s, tr, st,  'initial'));
 ReplenishSubstrate2
-save_workspace(strcat(tumor_name, 'ReplenishSubstrate.json'));
+save_workspace(sprintf('out-%s-tr%.0f-st%.0f-0b-%s.json',P_EVO.tum_lib_s, tr, st,  'ReplenishSubstrate'));
 
-for st = 1:PARAMS.n_st
-        Diffusion2
-save_workspace(strcat(tumor_name, 'Diffusion.json'));
-IrradiateTumor_evo(st,tau)
-save_workspace(strcat(tumor_name, 'IrradiateTumor.json'));
-SetLocalStates
-save_workspace(strcat(tumor_name, 'SetLocalStates.json'));
-MetaboliseNutrients
-save_workspace(strcat(tumor_name, 'MetaboliseNutrients.json'));
-SetGlobalStates
-save_workspace(strcat(tumor_name, 'SetGlobalStates.json'));
-RepairCells
-save_workspace(strcat(tumor_name, 'RepairCells.json'));
-CellDivision2
-save_workspace(strcat(tumor_name, 'CellDivision.json'));
-RunStats = GetStats2(st, P_EVO, RunStats);    % update RunStats
-save_workspace(strcat(tumor_name, 'RunStats.json'));
-FinishCheck
-save_workspace(strcat(tumor_name, 'FinishCheck.json'));
-if FINISH, return; end
-        end
+for st = 1:10
+   Diffusion2
+   save_workspace(sprintf('out-%s-tr%.0f-st%.0f-1-%s.json',P_EVO.tum_lib_s, tr, st,  'Diffusion'));
+   IrradiateTumor_evo(st,tau)
+   save_workspace(sprintf('out-%s-tr%.0f-st%.0f-2-%s.json',P_EVO.tum_lib_s, tr, st,  'IrradiateTumor'));
+   SetLocalStates
+   save_workspace(sprintf('out-%s-tr%.0f-st%.0f-3-%s.json',P_EVO.tum_lib_s, tr, st,  'SetLocalStates'));
+   MetaboliseNutrients
+   save_workspace(sprintf('out-%s-tr%.0f-st%.0f-4-%s.json',P_EVO.tum_lib_s, tr, st,  'MetaboliseNutrients'));
+   SetGlobalStates
+   save_workspace(sprintf('out-%s-tr%.0f-st%.0f-5-%s.json',P_EVO.tum_lib_s, tr, st,  'SetGlobalStates'));
+   RepairCells
+   save_workspace(sprintf('out-%s-tr%.0f-st%.0f-6-%s.json',P_EVO.tum_lib_s, tr, st,  'RepairCells'));
+   CellDivision2
+   save_workspace(sprintf('out-%s-tr%.0f-st%.0f-7-%s.json',P_EVO.tum_lib_s, tr, st,  'CellDivision'));
+   RunStats = GetStats2(st, P_EVO, RunStats);    % update RunStats
+   save_workspace(sprintf('out-%s-tr%.0f-st%.0f-8-%s.json',P_EVO.tum_lib_s, tr, st,  'RunStats'));
+   FinishCheck
+   save_workspace(sprintf('out-%s-tr%.0f-st%.0f-9-%s.json',P_EVO.tum_lib_s, tr, st,  'FinishCheck'));
+   if FINISH, return; end
+end
 
 quit
