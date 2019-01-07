70708b7f719386222656801022f32ad06b3e57fb
Rafał Banaś <Banas.Rafal97@gmail.com>
2019-01-07 21:38:30 +0100

    qf

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #include <RandomEngine.h>
 
 int main() {
-    MatlabRandomEngine mre(10009);
-    auto ca = Automaton::loadFromFile("../tests/resources/matlab_results/out-vnw-tr1-st0-0a-initial.json", &mre);
+    StdRandomEngine sre(10009);
+    auto ca = Automaton::loadFromFile("../tests/resources/matlab_results/out-vnw-tr1-st0-0a-initial.json", &sre);
     ca.runNSteps(4800);
     return 0;
 }
